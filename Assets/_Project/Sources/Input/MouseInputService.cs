70f2fb87de62ec17f9e68762a7e22b04d95aa395
nasusfen <ravshannedonat@gmail.com>
2024-10-11 14:46:38 +0300

    some changes

--- Assets/_Project/Sources/Input/MouseInputService.cs
+++ Assets/_Project/Sources/Input/MouseInputService.cs
@@ -3,17 +3,20 @@ using System;
 
 public class MouseInputService : MonoBehaviour
 {
+    private const int LeftMouseNumber = 0;
+    private const int RightMouseNumber = 1;
+    
     public event Action RMBClick = delegate { };
     public event Action LMBClick = delegate { };
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(LeftMouseNumber))
         {
             LMBClick.Invoke();
         }
         
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(RightMouseNumber))
         {
             RMBClick.Invoke();
         }
